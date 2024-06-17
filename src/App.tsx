3c977e036e1cefefd4e1d42959327018bc374bdd
dawitMesfin <dawitmesfin2019@gmail.com>
2024-06-17 10:08:28 +0300

    build expandable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
+import ExpandableComponent from './Components/ExpandableComponent'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <ExpandableComponent maxChar = {10}>
-      
+    
+    <ExpandableComponent >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Ducimus, molestiae alias odit voluptatibus asperiores cum harum quo 
+        error illum aliquid. Consequuntur numquam unde quasi 
+        dignissimos fugiat repellat nam quibusdam accusamus dolore. 
+        Ratione, excepturi. Necessitatibus, neque rem. Illum cupiditate 
+        reiciendis, sed vel aperiam consequuntur repellendus tenetur 
+        quam nobis inventore magni doloremque necessitatibus aliquid 
+        obcaecati error commodi distinctio veniam sunt voluptatem? 
+        Dignissimos doloribus obcaecati animi eum perferendis, omnis 
+        qui ducimus quasi, unde ipsam dolore laboriosam consectetur 
+        cupiditate. Placeat eligendi eius deserunt quam nostrum sequi 
+        eaque architecto deleniti neque iure, ab debitis, aut expedita!
+         Earum laboriosam aliquam odio? Consequuntur optio ab ipsam nam.
     </ExpandableComponent>
     </>
   )
--- src/Components/ExpandableComponent.tsx
+++ src/Components/ExpandableComponent.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react"
 
 
 
 interface ChildrenProps {
   children : string,
-  maxChar : number
+  maxChar? : number
 }
-const ExpandableComponent = ({ children , maxChar }: ChildrenProps) => {
+const ExpandableComponent = ({ children , maxChar = 100 }: ChildrenProps) => {
 
+const [isExpanded , setIsExpanded] = useState(false)
+const handleExpanison = () => {
+  setIsExpanded(!isExpanded)
+}
+const buttonLable = isExpanded ? 'less' : 'more'
 
+if(children.length <= maxChar) return <>{children}</>
+
+const text = isExpanded? children : children.substring(0,maxChar)
 
   return(
     <>
-    <p>{children}</p>
+    <p>{text}...<button onClick={handleExpanison}>{buttonLable}</button></p>
     </>
   )
 }
+export default ExpandableComponent
